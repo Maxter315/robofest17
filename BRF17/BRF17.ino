1ad77c75566bc436a1ed5018ab2cb2d89d6d3158
Max <Maxter315@gmail.com>
2017-05-31 16:42:09 +0300

    upd

--- BRF17/BRF17.ino
+++ BRF17/BRF17.ino
@@ -21,18 +21,22 @@ uint8_t pwm_l, pwm_r;
 uint8_t pwm_l_c, pwm_r_c;
 
 unsigned long prevMillis, currentMillis;
+
 int16_t s1,s2,s3,s4,s5;		//readings
 int16_t sens[5];			//readings
 uint8_t minSens, maxSens;
+uint16_t minVal, maxVal;
+uint16_t thrL, thrR;
 
 
 
 /* ----- Functions ----- */
+uint8_t sensInit(void);
 void readSensors(void);
 int16_t defineLine(void);
-int16_t reactPID(int16_t input);
+int8_t reactPID(int16_t input);
 uint8_t modeCtrl(void);
-void reactDRV(int16_t);
+void reactDRV(int8_t in);
 
 
 /* ----- INITIALIZATION ----- */
@@ -69,12 +73,35 @@ void loop(){
 
 
 /* ----- Functions ----- */
+uint8_t sensInit(void){
+	uint8_t out;
+
+	digitalWrite(LED1, HIGH);
+	delay(500);
+	digitalWrite(LED1, LOW);
+
+	readSensors();
+	minVal = sens[minSens];
+	maxVal = sens[maxSens];
+
+	if ((maxVal - minVal) < 100){
+		out = 1;
+	}
+
+	delay(10);
+	digitalWrite(LED1, HIGH);
+	delay(500);
+	digitalWrite(LED1, LOW);
+
+	return out;
+}
+
 void readSensors(void){
-	sens[0] = analogRead(A1);
-	sens[1] = analogRead(A2);
-	sens[2] = analogRead(A3);
-	sens[3] = analogRead(A4);
-	sens[4] = analogRead(A5);
+	sens[0] = 1024 - analogRead(A1);
+	sens[1] = 1024 - analogRead(A2);
+	sens[2] = 1024 - analogRead(A3);
+	sens[3] = 1024 - analogRead(A4);
+	sens[4] = 1024 - analogRead(A5);
 
 	minSens = 1;
 	maxSens = 1;
@@ -88,15 +115,19 @@ void readSensors(void){
 
 int16_t defineLine(void){
 	int16_t out;
+	
 	int16_t acc;
 	int16_t tmp[5];
+	int16_t minv;
+	minv = sens[minSens];
 
-	tmp[0] = sens[0] - sens[minSens];
-	tmp[1] = sens[1] - sens[minSens];
-	tmp[2] = sens[2] - sens[minSens];
-	tmp[3] = sens[3] - sens[minSens];
-	tmp[4] = sens[4] - sens[minSens];
+	//tmp[0] = sens[0] - minv;
+	//tmp[1] = sens[1] - minv;
+	//tmp[2] = sens[2] - minv;
+	//tmp[3] = sens[3] - minv;
+	//tmp[4] = sens[4] - minv;
 
+	/*
 	acc += tmp[0] * 0;
 	acc += tmp[1] * 15;
 	acc += tmp[2] * 30;
@@ -104,13 +135,15 @@ int16_t defineLine(void){
 	acc += tmp[4] * 60;
 
 	out = acc / (tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4]);
+	*/
+
 
 	return out;
 }
 
 
-int16_t reactPID(int16_t in){
-	int16_t out;
+int8_t reactPID(int16_t in){
+	int8_t out;
 
 	return out;
 }
@@ -123,7 +156,7 @@ uint8_t modeCtrl(void){
 }
 
 
-void reactDRV(int16_t sig){
+void reactDRV(int8_t sig){
 /*
 	if (sig > 0){
 		pwm_l = 0;
