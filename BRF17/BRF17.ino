aa2f3ea4938389064bb163796cb3c5554359a5eb
Maxter315 <Maxter315@gmail.com>
2017-05-31 22:50:56 +0300

    upd?

--- BRF17/BRF17.ino
+++ BRF17/BRF17.ino
@@ -2,9 +2,9 @@
 #include <stdint.h>
 //#include <Servo.h>
 
-#define DELTATIME 100
+#define DELTATIME 500
 
-#define LED1 7
+#define LED1 2
 #define LED2 8
 #define LED3 9
 #define LED4 10
@@ -14,8 +14,8 @@
 #define SLOPE_K 2
 
 /* ----- Variables & Objects ----- */
-AF_DCMotor leftMotor(2);
-AF_DCMotor rightMotor(1);
+AF_DCMotor leftMotor(3);
+AF_DCMotor rightMotor(4);
 
 uint8_t pwm_l, pwm_r;
 uint8_t pwm_l_c, pwm_r_c;
@@ -24,6 +24,7 @@ unsigned long prevMillis, currentMillis;
 
 int16_t s1,s2,s3,s4,s5;		//readings
 int16_t sens[5];			//readings
+float eq[5];
 uint8_t minSens, maxSens;
 uint16_t minVal, maxVal;
 uint16_t thrL, thrR;
@@ -42,6 +43,7 @@ void reactDRV(int8_t in);
 
 /* ----- INITIALIZATION ----- */
 void setup(){
+  Serial.begin(9600);
 /*
 	pinMode(A1, INPUT);
 	pinMode(A2, INPUT);
@@ -52,12 +54,19 @@ void setup(){
 	leftMotor.run(RELEASE);
 	rightMotor.run(RELEASE);
 
-	//pinMode(LED1, OUTPUT);
+	pinMode(LED1, OUTPUT);
 	//pinMode(LED2, OUTPUT);
 	//pinMode(LED3, OUTPUT);
 	//pinMode(LED4, OUTPUT);
 	//pinMode(LED5, OUTPUT);
 
+
+  //leftMotor.setSpeed(125);
+  //leftMotor.run(FORWARD);
+
+  //rightMotor.setSpeed(255);
+  //rightMotor.run(FORWARD);
+  
 	sensInit();
 }
 
@@ -68,10 +77,23 @@ void loop(){
 	if (currentMillis - prevMillis >= DELTATIME){
 		prevMillis = currentMillis;
 		
-		readSensors();
+		readSensors();/*
+  Serial.print(sens[0]); Serial.print("\t");
+  Serial.print(sens[1]); Serial.print("\t");
+  Serial.print(sens[2]); Serial.print("\t");
+  Serial.print(sens[3]); Serial.print("\t");
+  Serial.println(sens[4]);
 
+  Serial.print(eq[0]*((float)sens[0])); Serial.print("\t");
+  Serial.print(eq[1]*((float)sens[1])); Serial.print("\t");
+  Serial.print(eq[2]*((float)sens[2])); Serial.print("\t");
+  Serial.print(eq[3]*((float)sens[3])); Serial.print("\t");
+  Serial.println(eq[4]*((float)sens[4]));
+  
+  */
 		tmp_sig	= reactPID(defineLine());
-		reactDRV(tmp_sig);
+   Serial.println(tmp_sig);
+		//reactDRV(tmp_sig);
 
 	}
 }
@@ -90,13 +112,36 @@ uint8_t sensInit(void){
 	minVal = sens[minSens];
 	maxVal = sens[maxSens];
 
+  int16_t accum[5] = {0,0,0,0,0};
+
+for(int t = 0; t<10; t++){
+    readSensors();
+    for(int o = 0; o<5; o++){
+      accum[o] += sens[o];
+    }
+}
+
+for(int i = 0; i<5; i++){
+  eq[i] = ((float)accum[2])/((float)accum[i]);
+  Serial.print("eq[");
+  Serial.print(i);
+  Serial.print("]: ");
+  Serial.println(eq[i]);
+ 
+}
+
+
 	if ((maxVal - minVal) < 100){
 		out = 1;
 	}
 
+  Serial.print("min:");
+  Serial.print(minVal);
+  Serial.print("\tmax:");
+  Serial.println(maxVal);
 	delay(10);
 	digitalWrite(LED1, HIGH);
-	delay(500);
+	delay(1500);
 	digitalWrite(LED1, LOW);
 
 	return out;
@@ -122,31 +167,39 @@ void readSensors(void){
 int16_t defineLine(void){
 	int16_t out;
 	
-	int16_t acc;
-	int16_t tmp[5];
+	//int16_t acc;
+	//int16_t tmp[5];
+ float tmp[5], acc;
 	int16_t minv;
 	minv = sens[minSens];
+ if(minv> 200) minv = 200;
 
-	//tmp[0] = sens[0] - minv;
-	//tmp[1] = sens[1] - minv;
-	//tmp[2] = sens[2] - minv;
-	//tmp[3] = sens[3] - minv;
-	//tmp[4] = sens[4] - minv;
-
-	/*
+	tmp[0] = eq[0] * (float)(sens[0] - minv);
+	tmp[1] = eq[1] * (float)(sens[1] - minv);
+	tmp[2] = eq[2] * (float)(sens[2] - minv);
+	tmp[3] = eq[3] * (float)(sens[3] - minv);
+	tmp[4] = eq[4] * (float)(sens[4] - minv);
+/*
+  Serial.print(tmp[0]); Serial.print("\t");
+  Serial.print(tmp[1]); Serial.print("\t");
+  Serial.print(tmp[2]); Serial.print("\t");
+  Serial.print(tmp[3]); Serial.print("\t");
+  Serial.println(tmp[4]);
+*/
+  
 	acc += tmp[0] * 0;
 	acc += tmp[1] * 15;
 	acc += tmp[2] * 30;
 	acc += tmp[3] * 45;
 	acc += tmp[4] * 60;
 
-	out = acc / (tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4]);
-	*/
-
+	out = (uint16_t)((acc / (tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4])));
+	
+/*
 	if 		((sens[1] > 600) && (sens[3] < 500)) out = 32;
 	else if ((sens[3] > 600) && (sens[1] < 500)) out = -32;
 	else out = 0;
-
+*/
 	return out;
 }
 
